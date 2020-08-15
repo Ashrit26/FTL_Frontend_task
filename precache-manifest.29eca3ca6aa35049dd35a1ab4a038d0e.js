e077fd10be9578c40c29c818c3247f1ee03cdf81
oneashishc <ashritmr@rocketmail.com>
2020-08-15 20:39:12 +0530

    Updates

---
+++ precache-manifest.29eca3ca6aa35049dd35a1ab4a038d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e4f028500ce9483dda08d3a5d413e01",
+    "url": "/FTL_Frontend_task/index.html"
+  },
+  {
+    "revision": "dd531ed59579a80a3f03",
+    "url": "/FTL_Frontend_task/static/css/main.2c14ec1c.chunk.css"
+  },
+  {
+    "revision": "8a3f7989c002591b9ffe",
+    "url": "/FTL_Frontend_task/static/js/2.7727d826.chunk.js"
+  },
+  {
+    "revision": "6acd56cd61d2a383ebc1fb3b7f2e5590",
+    "url": "/FTL_Frontend_task/static/js/2.7727d826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd531ed59579a80a3f03",
+    "url": "/FTL_Frontend_task/static/js/main.ecb738a9.chunk.js"
+  },
+  {
+    "revision": "b956ff89d281a398c46e",
+    "url": "/FTL_Frontend_task/static/js/runtime-main.435b55b1.js"
+  }
+]);
